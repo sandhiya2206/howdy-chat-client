3afe80bf8dcaa1fbc314c5094c1e677e9162dbd4
SANDHIYA PALANI <sandhiya22062001@gmail.com>
2026-07-15 18:29:24 +0530

    Update axios.ts

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: `https://server-4jry.onrender.com/api`,
 });
 
 export default api;
